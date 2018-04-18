f2b2823293873b9ae51ee34551777f4e0a4725e8
Eagle Hawk <eagle.soaringheights@gmail.com>
2018-04-18 17:45:17 +0530

    Adding DELETE operation from CRUD

--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ var {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose-connect');
 var { Todo } = require('./models/todo');
 var { oUser } = require('./models/userInfo');
-const _PORT = process.env.PORT || 8080 ;
+const _PORT = process.env.PORT || 7000 ;
 
 var app = express();
 
 app.set('port', _PORT);
 app.use(bodyParser.json());
 
+// REST Create Operation
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
@@ -24,6 +25,7 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+// REST READ Operation
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({ todos });
@@ -49,6 +51,7 @@ app.post('/oUser', (req, res) => {
 		})
 }) ;
 
+// REST READ operation using a resource conditions
 app.get('/todos/:id', (req, res) => {
 	// GET Route for fetching an individial resource by using a request.
 	let oID = req.params.id;
@@ -65,7 +68,30 @@ app.get('/todos/:id', (req, res) => {
 	}).catch(erro => {
 		res.status(400).send('Could not find the document. Error -> ', erro)
 	})
-})
+}) ;
+
+// REST DELETE operation using the resource condition.
+app.delete('/todos', (req, res) => {
+	// DELETE Route for deleting a individial or multiple documents using a request.
+	let oID = req.params.id ;
+
+	console.log(`Request params : ${JSON.stringify(req.params, undefined, 2)} `);
+	
+	if (!ObjectID.isValid(oID)) {
+		return res.status(404).send() ;
+	}
+
+	Todo.findByIdAndRemove({_id: oID}).then( result => {
+		if (!result) {
+			return res.status(404).send();
+		}
+		res.status(200).send(`Data sucessfully Deleted : ${result} `)
+	} ).catch(error => {
+		res.status(400).send('Could not find the document. Error -> ', error) ;
+	})
+}) ;
+
+
 
 app.listen(_PORT, () => {
 	console.log(`Started on port ${_PORT}`);
